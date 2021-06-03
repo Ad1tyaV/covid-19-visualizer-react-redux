28a3ca6e627ea15e053aa00f584d938d00fa6639
AdithyaVadrevu <adithya.vadrevu@gmail.com>
2021-06-03 18:39:11 -0400

    Update totalDataAction.js

--- src/redux/actions/totalDataAction.js
+++ src/redux/actions/totalDataAction.js
@@ -1,7 +1,8 @@
 const addData=()=>{
     
     return async(dispatch)=>{
-        const data = await fetch(`https://api.covidtracking.com/v1/us/daily.json`);        
+        //const data = await fetch(`https://api.covidtracking.com/v1/us/daily.json`);        
+		const data = await fetch(`https://raw.githubusercontent.com/Ad1tyaV/pyTestFiles/master/daily.json`);    
         var jsonData = await data.json();
         //console.log(jsonData);
         dispatch({type:'ADD_DATA',payload:jsonData})
